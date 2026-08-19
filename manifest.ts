fa22ac10df91edb52db7f9cb4ad581dba2304068
byrkedavid <36374904+byrkedavid@users.noreply.github.com>
2026-08-19 08:52:18 -0400

    Add Slack-hosted app manifest

---
+++ manifest.ts
@@ -0,0 +1,23 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import CheckInWorkflow from "./workflows/check_in.ts";
+import CurrentCheckins from "./datastores/current_checkins.ts";
+import CheckinHistory from "./datastores/checkin_history.ts";
+import Users from "./datastores/users.ts";
+import AppState from "./datastores/app_state.ts";
+
+export default Manifest({
+  name: "Onsite",
+  description: "Slack-native onsite location check-ins and a living team summary.",
+  icon: "static/apple-touch-icon.png",
+  outgoingDomains: [],
+  workflows: [CheckInWorkflow],
+  datastores: [CurrentCheckins, CheckinHistory, Users, AppState],
+  botScopes: [
+    "commands",
+    "chat:write",
+    "chat:write.public",
+    "datastore:read",
+    "datastore:write",
+    "users:read",
+  ],
+});
