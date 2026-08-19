f02d105a9f8c1f256446815f85b98b3ed8a141d7
byrkedavid <36374904+byrkedavid@users.noreply.github.com>
2026-08-19 10:27:51 -0400

    Register daily onsite prompt workflow

--- manifest.ts
+++ manifest.ts
@@ -1,6 +1,7 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
 import CheckInWorkflow from "./workflows/check_in.ts";
 import ResetDailyWorkflow from "./workflows/reset_daily.ts";
+import DailyPromptWorkflow from "./workflows/daily_prompt.ts";
 import CurrentCheckins from "./datastores/current_checkins.ts";
 import CheckinHistory from "./datastores/checkin_history.ts";
 import Users from "./datastores/users.ts";
@@ -8,9 +9,9 @@ import AppState from "./datastores/app_state.ts";
 
 export default Manifest({
   name: "Onsite",
-  description: "Slack-native onsite location check-ins and a living team view.",
+  description: "Slack-native onsite location check-ins with a live team Canvas.",
   outgoingDomains: [],
-  workflows: [CheckInWorkflow, ResetDailyWorkflow],
+  workflows: [CheckInWorkflow, ResetDailyWorkflow, DailyPromptWorkflow],
   datastores: [CurrentCheckins, CheckinHistory, Users, AppState],
   botScopes: [
     "commands",
