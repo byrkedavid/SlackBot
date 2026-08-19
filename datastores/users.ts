20be17ea2096ef9bb78ad63148780362f64fcd3d
byrkedavid <36374904+byrkedavid@users.noreply.github.com>
2026-08-19 08:49:24 -0400

    Add tracked users datastore

---
+++ datastores/users.ts
@@ -0,0 +1,15 @@
+import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
+
+const Users = DefineDatastore({
+  name: "users",
+  primary_key: "user_id",
+  attributes: {
+    user_id: { type: Schema.slack.types.user_id },
+    display_name: { type: Schema.types.string },
+    image_url: { type: Schema.types.string },
+    schedule_type: { type: Schema.types.string },
+    updated_at: { type: Schema.types.string },
+  },
+});
+
+export default Users;
