ff521efc09734bfbea1ef6ef64808f20f27d057d
byrkedavid <36374904+byrkedavid@users.noreply.github.com>
2026-08-19 11:03:35 -0400

    Track POC role for onsite users

--- datastores/users.ts
+++ datastores/users.ts
@@ -6,6 +6,8 @@ const Users = DefineDatastore({
   attributes: {
     user_id: { type: Schema.slack.types.user_id },
     display_name: { type: Schema.types.string },
+    slack_username: { type: Schema.types.string },
+    is_poc: { type: Schema.types.boolean },
     image_url: { type: Schema.types.string },
     updated_at: { type: Schema.types.string },
   },
